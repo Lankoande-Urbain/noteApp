26a4d5fd7950629680c00245296963b3b07aeb44
Lankoande Urbain <lankoandeurbain8.gmail.com>
2024-04-16 15:44:06 +0000

    Implementation de AppIcon et SplashScreen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Routes from './src/routes'
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+  useEffect(() =>{
+    setTimeout(() =>{
+      SplashScreen.hide()
+    }, 500)
+  })
   return (
     <View style={{ flex:1 }}>
       <Routes/>
